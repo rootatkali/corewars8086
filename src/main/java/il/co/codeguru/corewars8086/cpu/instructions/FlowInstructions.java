bcdbf995857f3410ae43f6e1ab524dfd4f6bde1a
Rotem Moses <rootatkali4@gmail.com>
2023-06-03 13:17:51 +0300

    WIP Migrate opcodes 0xE0-0xEF
    Opcodes 0xE4-0xE7, 0xEC-0xEF are unsupported

--- src/main/java/il/co/codeguru/corewars8086/cpu/instructions/FlowInstructions.java
+++ src/main/java/il/co/codeguru/corewars8086/cpu/instructions/FlowInstructions.java
@@ -2,11 +2,9 @@ package il.co.codeguru.corewars8086.cpu.instructions;
 
 import il.co.codeguru.corewars8086.cpu.Instruction;
 import il.co.codeguru.corewars8086.cpu.InstructionResolver;
-import il.co.codeguru.corewars8086.cpu.NgCpuState;
-import il.co.codeguru.corewars8086.cpu.NgOpcodeFetcher;
-import il.co.codeguru.corewars8086.memory.MemoryException;
 
 import static il.co.codeguru.corewars8086.cpu.instructions.FlowUtils.callFar;
+import static il.co.codeguru.corewars8086.cpu.instructions.FlowUtils.callNear;
 
 public class FlowInstructions {
     public static final InstructionResolver FLOW_INSTRUCTIONS = new InstructionResolver();
@@ -15,108 +13,98 @@ public class FlowInstructions {
      * 0x70 - JO
      */
     private static final Instruction JO = (state, memory, opcodeFetcher, registers, addressingDecoder)
-            -> conditionalJump(state, opcodeFetcher, state.getOverflowFlag());
+            -> FlowUtils.conditionalJump(state, opcodeFetcher, state.getOverflowFlag());
 
     /**
      * 0x71 - JNO
      */
     private static final Instruction JNO = (state, memory, opcodeFetcher, registers, addressingDecoder)
-            -> conditionalJump(state, opcodeFetcher, !state.getOverflowFlag());
+            -> FlowUtils.conditionalJump(state, opcodeFetcher, !state.getOverflowFlag());
 
     /**
      * 0x72 - JC, JB, JNAE
      */
     private static final Instruction JC_JB_JNAE = (state, memory, opcodeFetcher, registers, addressingDecoder)
-            -> conditionalJump(state, opcodeFetcher, state.getCarryFlag());
+            -> FlowUtils.conditionalJump(state, opcodeFetcher, state.getCarryFlag());
 
     /**
      * 0x73 - JNC, JNB, JAE
      */
     private static final Instruction JNC_JNB_JAE = (state, memory, opcodeFetcher, registers, addressingDecoder)
-            -> conditionalJump(state, opcodeFetcher, !state.getCarryFlag());
+            -> FlowUtils.conditionalJump(state, opcodeFetcher, !state.getCarryFlag());
 
     /**
      * 0x74 - JE, JZ
      */
     private static final Instruction JE_JZ = (state, memory, opcodeFetcher, registers, addressingDecoder)
-            -> conditionalJump(state, opcodeFetcher, state.getZeroFlag());
+            -> FlowUtils.conditionalJump(state, opcodeFetcher, state.getZeroFlag());
 
     /**
      * 0x75 - JNE, JNZ
      */
     private static final Instruction JNE_JNZ = (state, memory, opcodeFetcher, registers, addressingDecoder)
-            -> conditionalJump(state, opcodeFetcher, !state.getZeroFlag());
+            -> FlowUtils.conditionalJump(state, opcodeFetcher, !state.getZeroFlag());
 
     /**
      * 0x76 - JBE, JNA
      */
     private static final Instruction JBE_JNA = (state, memory, opcodeFetcher, registers, addressingDecoder)
-            -> conditionalJump(state, opcodeFetcher, (state.getCarryFlag() || state.getZeroFlag()));
+            -> FlowUtils.conditionalJump(state, opcodeFetcher, (state.getCarryFlag() || state.getZeroFlag()));
 
     /**
      * 0x77 - JNBE, JA
      */
     private static final Instruction JNBE_JA = (state, memory, opcodeFetcher, registers, addressingDecoder)
-            -> conditionalJump(state, opcodeFetcher, !(state.getCarryFlag() || state.getZeroFlag()));
+            -> FlowUtils.conditionalJump(state, opcodeFetcher, !(state.getCarryFlag() || state.getZeroFlag()));
 
     /**
      * 0x78 - JS
      */
     private static final Instruction JS = (state, memory, opcodeFetcher, registers, addressingDecoder)
-            -> conditionalJump(state, opcodeFetcher, state.getSignFlag());
+            -> FlowUtils.conditionalJump(state, opcodeFetcher, state.getSignFlag());
 
     /**
      * 0x79 - JNS
      */
     private static final Instruction JNS = (state, memory, opcodeFetcher, registers, addressingDecoder)
-            -> conditionalJump(state, opcodeFetcher, !state.getSignFlag());
+            -> FlowUtils.conditionalJump(state, opcodeFetcher, !state.getSignFlag());
 
     /**
      * 0x7A - JP, JPE
      */
     private static final Instruction JP_JPE = (state, memory, opcodeFetcher, registers, addressingDecoder)
-            -> conditionalJump(state, opcodeFetcher, state.getParityFlag());
+            -> FlowUtils.conditionalJump(state, opcodeFetcher, state.getParityFlag());
 
     /**
      * 0x7B - JNP, JPO
      */
     private static final Instruction JNP_JPO = (state, memory, opcodeFetcher, registers, addressingDecoder)
-            -> conditionalJump(state, opcodeFetcher, !state.getParityFlag());
+            -> FlowUtils.conditionalJump(state, opcodeFetcher, !state.getParityFlag());
 
     /**
      * 0x7C - JL, JNGE
      */
     private static final Instruction JL_JNGE = (state, memory, opcodeFetcher, registers, addressingDecoder)
-            -> conditionalJump(state, opcodeFetcher, (state.getSignFlag() != state.getOverflowFlag()));
+            -> FlowUtils.conditionalJump(state, opcodeFetcher, (state.getSignFlag() != state.getOverflowFlag()));
 
     /**
      * 0x7D - JNL, JGE
      */
     private static final Instruction JNL_JGE = (state, memory, opcodeFetcher, registers, addressingDecoder)
-            -> conditionalJump(state, opcodeFetcher, (state.getSignFlag() == state.getOverflowFlag()));
+            -> FlowUtils.conditionalJump(state, opcodeFetcher, (state.getSignFlag() == state.getOverflowFlag()));
 
     /**
      * 0x7E - JLE, JNG
      */
     private static final Instruction JLE_JNG = (state, memory, opcodeFetcher, registers, addressingDecoder)
-            -> conditionalJump(state, opcodeFetcher, state.getZeroFlag() || (state.getSignFlag() != state.getOverflowFlag()));
+            -> FlowUtils.conditionalJump(state, opcodeFetcher, state.getZeroFlag() || (state.getSignFlag() != state.getOverflowFlag()));
 
     /**
      * 0x7F - JNLE, JG
      */
     private static final Instruction JNLE_JG = (state, memory, opcodeFetcher, registers, addressingDecoder)
-            -> conditionalJump(state, opcodeFetcher, !state.getZeroFlag() && (state.getSignFlag() == state.getOverflowFlag()));
-
-    private static void conditionalJump(NgCpuState state,
-                                        NgOpcodeFetcher opcodeFetcher,
-                                        boolean branch) throws MemoryException {
-        byte offset = opcodeFetcher.nextByte();
-
-        if (branch) {
-            state.setIp((short) (state.getIp() + offset));
-        }
-    }
-
+            -> FlowUtils.conditionalJump(state, opcodeFetcher, !state.getZeroFlag() && (state.getSignFlag() == state.getOverflowFlag()));
+    
     /**
      * 0x9A - CALL far imm16:imm16
      */
@@ -124,6 +112,64 @@ public class FlowInstructions {
         short newIp = opcodeFetcher.nextWord();
         short newCs = opcodeFetcher.nextWord();
         callFar(state, memory, newCs, newIp);
+    };
+    
+    /**
+     * 0xE0 - LOOPNZ, LOOPNE
+     */
+    private static final Instruction LOOPNZ_LOOPNE = (state, memory, opcodeFetcher, registers, addressingDecoder)
+            -> FlowUtils.conditionalLoop(state, opcodeFetcher, !state.getZeroFlag());
+    
+    /**
+     * 0xE1 - LOOPZ, LOOPE
+     */
+    private static final Instruction LOOPZ_LOOPE = (state, memory, opcodeFetcher, registers, addressingDecoder)
+            -> FlowUtils.conditionalLoop(state, opcodeFetcher, state.getZeroFlag());
+    
+    /**
+     * 0xE2 - LOOP
+     */
+    private static final Instruction LOOP = (state, memory, opcodeFetcher, registers, addressingDecoder)
+            -> FlowUtils.loop(state, opcodeFetcher);
+    
+    /**
+     * 0xE3 - JCXZ
+     */
+    private static final Instruction JCXZ = (state, memory, opcodeFetcher, registers, addressingDecoder)
+            -> FlowUtils.conditionalJump(state, opcodeFetcher, state.getCx() == 0);
+    
+    /**
+     * 0xE8 - CALL near imm16
+     */
+    private static final Instruction CALL_NEAR_IMM16 = (state, memory, opcodeFetcher, registers, addressingDecoder) -> {
+        short offset = opcodeFetcher.nextWord();
+        callNear(state, memory, (short) (state.getIp() + offset));
+    };
+    
+    /**
+     * 0xE9 - JMP near imm16
+     */
+    private static final Instruction JMP_NEAR_IMM16 = (state, memory, opcodeFetcher, registers, addressingDecoder) -> {
+        short offset = opcodeFetcher.nextWord();
+        state.setIp((short) (state.getIp() + offset));
+    };
+    
+    /**
+     * 0xEA - JMP far imm16:imm16
+     */
+    private static final Instruction JMP_FAR_IMM16_IMM16 = (state, memory, opcodeFetcher, registers, addressingDecoder) -> {
+        short newIp = opcodeFetcher.nextWord();
+        short newCs = opcodeFetcher.nextWord();
+        state.setIp(newIp);
+        state.setCs(newCs);
+    };
+    
+    /**
+     * 0xEB - JMP short imm8
+     */
+    private static final Instruction JMP_SHORT_IMM8 = (state, memory, opcodeFetcher, registers, addressingDecoder) -> {
+        byte offset = opcodeFetcher.nextByte();
+        state.setIp((short) (state.getIp() + offset));
     };
 
     static {
@@ -145,5 +191,14 @@ public class FlowInstructions {
         FLOW_INSTRUCTIONS.add((byte) 0x7F, JNLE_JG);
 
         FLOW_INSTRUCTIONS.add((byte) 0x9A, CALL_FAR_IMM32);
+        
+        FLOW_INSTRUCTIONS.add((byte) 0xE0, LOOPNZ_LOOPNE);
+        FLOW_INSTRUCTIONS.add((byte) 0xE1, LOOPZ_LOOPE);
+        FLOW_INSTRUCTIONS.add((byte) 0xE2, LOOP);
+        FLOW_INSTRUCTIONS.add((byte) 0xE3, JCXZ);
+        FLOW_INSTRUCTIONS.add((byte) 0xE8, CALL_NEAR_IMM16);
+        FLOW_INSTRUCTIONS.add((byte) 0xE9, JMP_NEAR_IMM16);
+        FLOW_INSTRUCTIONS.add((byte) 0xEA, JMP_FAR_IMM16_IMM16);
+        FLOW_INSTRUCTIONS.add((byte) 0xEB, JMP_SHORT_IMM8);
     }
 }
--- src/main/java/il/co/codeguru/corewars8086/cpu/instructions/FlowUtils.java
+++ src/main/java/il/co/codeguru/corewars8086/cpu/instructions/FlowUtils.java
@@ -1,6 +1,7 @@
 package il.co.codeguru.corewars8086.cpu.instructions;
 
 import il.co.codeguru.corewars8086.cpu.NgCpuState;
+import il.co.codeguru.corewars8086.cpu.NgOpcodeFetcher;
 import il.co.codeguru.corewars8086.memory.MemoryException;
 import il.co.codeguru.corewars8086.memory.RealModeMemory;
 
@@ -14,5 +15,31 @@ public class FlowUtils {
     static void callNear(NgCpuState state, RealModeMemory memory, short offset) throws MemoryException {
         StackUtils.push(state, memory, state.getIp());
         state.setIp(offset);
+    }
+    
+    static void conditionalJump(NgCpuState state,
+                                NgOpcodeFetcher opcodeFetcher,
+                                boolean branch) throws MemoryException {
+        byte offset = opcodeFetcher.nextByte();
+
+        if (branch) {
+            state.setIp((short) (state.getIp() + offset));
+        }
+    }
+    
+    static void conditionalLoop(NgCpuState state,
+                                NgOpcodeFetcher opcodeFetcher,
+                                boolean loop) throws MemoryException {
+        byte offset = opcodeFetcher.nextByte();
+        short newCx = (short) (state.getCx() - 1);
+        state.setCx(newCx);
+        
+        if (newCx != 0 && loop) {
+            state.setIp((short) (state.getIp() + offset));
+        }
+    }
+    
+    static void loop(NgCpuState state, NgOpcodeFetcher opcodeFetcher) throws MemoryException {
+        conditionalLoop(state, opcodeFetcher, true);
     }
 }
